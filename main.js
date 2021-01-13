c4ea0eede6d7ccb9b5a38806602e70106418db0a
= <roulit77@gmail.com>
2021-01-13 22:42:41 +0100

    base is done

---
+++ main.js
@@ -0,0 +1,25 @@
+module.exports = {
+    arraySum: function(arr) {
+        // Your code goes here
+    
+        return arr; //or something else who knows ?
+    },
+
+    arrayAverage: function(arr) {
+        // Your code goes here
+
+        return arr; // or something else who knows ?
+    },
+
+    arrayMax: function(arr) {
+        // Your code goes here
+
+        return arr // or something else who knows ?
+    },
+
+    arrayMin: function(arr) {
+        // Your code goes here
+
+        return arr // or something else who knows ?
+    }
+ };
