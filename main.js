8b467875f705e2edfe6ec65a750bedf7d6a69f66
= <roulit77@gmail.com>
2021-01-14 00:40:20 +0100

    step2-solution done

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 module.exports = {
     arraySum: function(arr) {
-        // Your code goes here
-    
-        return arr; //or something else who knows ?
+        if(arguments.length == 0) return 0;
+        let numOrChar = n => isNaN(n) ? 0 : parseInt(n);        
+        return arr.reduce((a, b) => numOrChar(a) + numOrChar(b));
     },
 
     arrayAverage: function(arr) {
-        // Your code goes here
-
-        return arr; // or something else who knows ?
+        if(arguments.length == 0) return 0;
+        const total = module.exports.arraySum(arr);
+        arr.forEach((e, i) => isNaN(e) && arr.splice(i, 1));
+        return total / arr.length;
     },
 
     arrayMax: function(arr) {
-        // Your code goes here
-
-        return arr // or something else who knows ?
+        if(arguments.length == 0) return 0;
+        let numOrChar = n => isNaN(n) ? false : parseInt(n);
+        return arr.reduce((a, b) => Math.max(numOrChar(a), numOrChar(b)));
     },
 
     arrayMin: function(arr) {
-        // Your code goes here
-
-        return arr // or something else who knows ?
+        if(arguments.length == 0) return 0;
+        arr = arr.filter((i) => !isNaN(i));
+        return arr.length === 0 ? 0 : Math.min(...arr);
     }
  };
